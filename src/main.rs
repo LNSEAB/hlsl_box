266e40783659e58cadeea5b735f7b26b8cc2473d
LNSEAB <691752+LNSEAB@users.noreply.github.com>
2022-04-19 17:04:34 +0900

    パニックをメッセージボックスで表示

--- src/main.rs
+++ src/main.rs
@@ -45,19 +45,31 @@ fn main() {
         libc::setlocale(libc::LC_ALL, b"\0".as_ptr() as _);
     }
     logger();
+    std::panic::set_hook(Box::new(|info| unsafe {
+        use windows::Win32::Foundation::HWND;
+        use windows::Win32::UI::WindowsAndMessaging::*;
+        let msg = match info.payload().downcast_ref::<&str>() {
+            Some(msg) => msg,
+            None => "unknown error",
+        };
+        MessageBoxW(HWND(0), msg, "HLSLBox", MB_OK | MB_ICONERROR);
+        error!("panic: {}", msg);
+    }));
     let _coinit = coinit::init(coinit::APARTMENTTHREADED | coinit::DISABLE_OLE1DDE).unwrap();
     let (th_tx, th_rx) = mpsc::channel();
     info!("start");
     let f = move || -> anyhow::Result<Window> {
         let settings = Arc::new(Settings::load(SETTINGS_PATH)?);
-        let (window, window_receiver) = Window::new(settings.clone())?;
+        let (window, window_receiver) = Window::new(settings.clone()).unwrap();
         let th_settings = settings.clone();
         let th = std::thread::spawn(move || {
             info!("start rendering thread");
             let _coinit = coinit::init(coinit::MULTITHREADED | coinit::DISABLE_OLE1DDE).unwrap();
+            let main_window = window_receiver.main_window.clone();
             let app = Application::new(th_settings, window_receiver).and_then(|mut app| app.run());
             if let Err(e) = app {
-                error!("{}\n{}", e, e.backtrace());
+                main_window.close();
+                panic!("{}\n{}", e, e.backtrace());
             }
             info!("end rendering thread");
         });
