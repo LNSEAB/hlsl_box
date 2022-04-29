a46cccf12e06efcfd909423f848b47a9e6d8154e
LNSEAB <691752+LNSEAB@users.noreply.github.com>
2022-04-30 00:51:22 +0900

    追加

--- package.ps1
+++ package.ps1
@@ -3,11 +3,13 @@ $dir = "package/hlsl_box"
 cargo.exe build --profile production
 
 if (Test-Path -Path $dir) {
-    Remove-Item -Recurse -Force -Path $dir
+    Remove-Item -Recurse -Force -Path $dir | Out-Null
 }
-New-Item $dir -ItemType Directory
+New-Item $dir -ItemType Directory | Out-Null
 Copy-Item "dxcompiler.dll" $dir
 Copy-Item "dxil.dll" $dir
 Copy-Item "target/production/hlsl_box.exe" $dir
 Copy-Item -Recurse -Force "include" $dir 
+Copy-Item -Recurse -Force "examples" $dir 
 Compress-Archive -Force -Path $dir -DestinationPath "package/hlsl_box.zip"
+Write-Host "finish package.ps1"
