f3e344917f0b175908c010db1d98bf7bac46d5d7
LNSEAB <691752+LNSEAB@users.noreply.github.com>
2022-05-04 08:04:05 +0900

    mainの切り分け

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,7 @@ static SETTINGS_PATH: Lazy<std::path::PathBuf> = Lazy::new(|| {
     EXE_DIR_PATH.join("settings.toml")
 });
 
-fn logger() {
+fn set_logger() {
     use std::fs::File;
     use tracing_subscriber::{filter::LevelFilter, prelude::*};
 
@@ -79,27 +79,22 @@ fn logger() {
         .init();
 }
 
-fn main() {
-    unsafe {
-        let locale = std::ffi::CString::new(LOCALE.as_ref().map_or("", |l| l.as_str())).unwrap();
-        libc::setlocale(libc::LC_ALL, locale.as_ptr());
-    }
-    logger();
-    std::panic::set_hook(Box::new(|info| unsafe {
-        use windows::Win32::Foundation::HWND;
-        use windows::Win32::UI::WindowsAndMessaging::*;
-        let msg = info
-            .payload()
-            .downcast_ref::<String>()
-            .map(|s| s.as_str())
-            .or_else(|| info.payload().downcast_ref::<&str>().copied());
-        match msg {
-            Some(msg) => {
-                let s = match info.location() {
-                    Some(loc) => format!("{} ({}:{})", msg, loc.file(), loc.line()),
-                    None => msg.to_string(),
-                };
-                if !ENV_ARGS.nomodal {
+fn panic_handler(info: &std::panic::PanicInfo) {
+    use windows::Win32::Foundation::HWND;
+    use windows::Win32::UI::WindowsAndMessaging::*;
+    let msg = info
+        .payload()
+        .downcast_ref::<String>()
+        .map(|s| s.as_str())
+        .or_else(|| info.payload().downcast_ref::<&str>().copied());
+    match msg {
+        Some(msg) => {
+            let s = match info.location() {
+                Some(loc) => format!("{} ({}:{})", msg, loc.file(), loc.line()),
+                None => msg.to_string(),
+            };
+            if !ENV_ARGS.nomodal {
+                unsafe {
                     MessageBoxW(
                         HWND(0),
                         s.as_str(),
@@ -107,19 +102,32 @@ fn main() {
                         MB_OK | MB_ICONERROR | MB_SYSTEMMODAL,
                     );
                 }
-                error!("panic: {}", s);
             }
-            None => {
-                let e = Error::UnknownError;
-                match info.location() {
-                    Some(loc) => error!("panic: {} ({}:{})", e, loc.file(), loc.line()),
-                    None => error!("panic: {}", e),
-                }
+            error!("panic: {}", s);
+        }
+        None => {
+            let e = Error::UnknownError;
+            match info.location() {
+                Some(loc) => error!("panic: {} ({}:{})", e, loc.file(), loc.line()),
+                None => error!("panic: {}", e),
             }
-        };
-    }));
+        }
+    };
+}
+
+fn set_locale() {
+    unsafe {
+        let locale = std::ffi::CString::new(LOCALE.as_ref().map_or("", |l| l.as_str())).unwrap();
+        libc::setlocale(libc::LC_ALL, locale.as_ptr());
+    }
+}
+
+fn main() {
+    set_logger();
+    std::panic::set_hook(Box::new(|info| panic_handler(info)));
     info!("start");
     debug!("ENV_ARGS: {:?}", &*ENV_ARGS);
+    set_locale();
     let _coinit = coinit::init(coinit::APARTMENTTHREADED | coinit::DISABLE_OLE1DDE).unwrap();
     let th_handle = Rc::new(RefCell::new(None));
     let th_handle_f = th_handle.clone();
