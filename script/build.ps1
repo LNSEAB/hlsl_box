f5dfc1d17a1b4646b2d2206719e08dcfeeb028d1
LNSEAB <691752+LNSEAB@users.noreply.github.com>
2022-05-13 16:45:16 +0900

    修正

--- script/build.ps1
+++ script/build.ps1
@@ -11,8 +11,6 @@ if ($args.Contains("release")) {
 } else {
     $target_dir = "$root/../target/debug"
     cargo.exe build
-    Copy-Item "$dxc_bin/dxcompiler.dll" "$target_dir/deps"
-    Copy-Item "$dxc_bin/dxil.dll" "$target_dir/deps"
 }
 Copy-Item "$dxc_bin/dxcompiler.dll" $target_dir
 Copy-Item "$dxc_bin/dxil.dll" $target_dir
