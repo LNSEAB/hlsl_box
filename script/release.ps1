70be5135abbf06c2122b690e2f3b00fc38e83efb
LNSEAB <691752+LNSEAB@users.noreply.github.com>
2022-05-18 08:49:50 +0900

    修正

--- script/release.ps1
+++ script/release.ps1
@@ -1,4 +1,13 @@
+if ($null -eq $args[1]) {
+    Write-Host "invalid argument"
+    exit 1
+}
+
 git checkout -b staging
-cargo.exe release $args[1] --execute
+cargo.exe release $args[1]
+if ($?) {
+    exit 1
+}
 git push -u origin staging
+git checkout master
 git branch -D staging
