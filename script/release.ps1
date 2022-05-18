0eb7cf6a5aa704300cd6f34077c4cbeea282f942
LNSEAB <691752+LNSEAB@users.noreply.github.com>
2022-05-18 09:16:53 +0900

    修正

--- script/release.ps1
+++ script/release.ps1
@@ -1,13 +1,20 @@
-if ($null -eq $args[1]) {
+if ($null -eq $Args[0]) {
     Write-Host "invalid argument"
     exit 1
 }
 
-git checkout -b staging
-cargo.exe release $args[1]
+$branch = "staging"
+$prev_branch = git branch --contains | ForEach-Object {$($_ -split(" "))[1]}
+
+git checkout -b $branch
+cargo.exe release $Args[0]
 if ($?) {
+    git checkout Cargo.lock
+    git checkout Cargo.toml
+    git checkout $prev_branch 
+    git branch -d $branch
     exit 1
 }
-git push -u origin staging
-git checkout master
-git branch -D staging
+git push -u origin $branch
+git checkout $prev_branch
+git branch -D $branch
